ad0f5b8704adf7cfaadba480feb0ab40f77e88d8
Stanislav Parkhomenko <stanislav.parkhomenko@gmail.com>
2025-02-19 10:53:23 +0200

    updated validator config (#93)
    * updated validator config
    
    * docker env changes

--- entrypoint-validator.sh
+++ entrypoint-validator.sh
@@ -3,14 +3,15 @@
 network=finney
 netuid=50
 
-vpermit_tao_limit=30000
+vpermit_tao_limit=999999
 
 validator_coldkey_name=validator
 validator_hotkey_name=default
 
-ewma_alpha=2.0
-ewma_half_life_days=1.0
-ewma_cutoff_days=2
+ewma_alpha=4.0
+ewma_half_life_days=2.0
+ewma_cutoff_days=4
+softmax_beta=-0.002
 
 wandb_enabled=false
 wandb_project_name=my_wandb_project
@@ -26,6 +27,8 @@ python3.10 ./neurons/validator.py \
 		--ewma.alpha $ewma_alpha \
 		--ewma.half_life_days $ewma_half_life_days \
 		--ewma.cutoff_days $ewma_cutoff_days \
+		--ewma.standard_ma_disabled \
+		--softmax.beta $softmax_beta \
 		--neuron.vpermit_tao_limit $vpermit_tao_limit \
 		--wandb.enabled $wandb_enabled \
 		--wandb.project_name $wandb_project_name \
